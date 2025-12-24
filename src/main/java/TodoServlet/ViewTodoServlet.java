94082060b37175529caf41a187bb1d844c821332
Mouad Oumous <mouadoumous@gmail.com>
2025-12-24 18:39:54 +0100

    add view page

---
+++ src/main/java/TodoServlet/ViewTodoServlet.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package TodoServlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet("/todo/view")
+public class ViewTodoServlet extends HttpServlet {
+
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        Integer id;
+
+        System.out.println(request.getParameter("id"));
+        try {
+            id = Integer.valueOf(request.getParameter("id"));
+        } catch (NumberFormatException e) {
+            response.sendRedirect(request.getContextPath()+ "/todos");
+            return;
+        }
+        
+        Todo todo = TodoStore.getTodo(id);
+        
+        if (todo == null) {
+            response.sendRedirect(request.getContextPath()+ "/todos");
+            return;
+        }
+        
+        request.setAttribute("todo", todo);
+        request.getRequestDispatcher("/ViewTodo.jsp").forward(request, response);
+       
+    }
+    
+}
---
+++ src/main/webapp/ViewTodo.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : ViewTodo.jsp
+    Created on : Dec 24, 2025, 5:11:56 PM
+    Author     : admin
+--%>
+
+
+
+<%@page import="TodoServlet.Todo"%>
+<%@ include file="header.jsp"%>
+
+<h3>View Todo</h3>
+
+<%
+    Todo todo = (Todo) request.getAttribute("todo");
+%>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><%= todo.getTitle() %></h5>
+        <p class="card-text"><%= todo.getDescription() %></p>
+
+        <a href="${request.getContextPath()+"/todos"}" class="btn btn-secondary">Back</a>
+    </div>
+</div>
